1e0e7aa0e79eda43d5d3f35284059bf637187831
Gary Tsai <gary.m.tsai@gmail.com>
2013-09-21 11:23:46 -0700

    uhhhh

---
+++ teamAwesome.js
@@ -0,0 +1,13 @@
+(function(){
+			var name = "Gary"
+			var name2 = "Nathan"
+			var name3 = "Christian"
+
+			var names = function(){
+				return name + " " + name2 + " " + name3
+			}
+
+	coolNames = names
+
+	}
+)()
